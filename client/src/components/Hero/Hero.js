e8e1c9d775b3ee6bbc24694160cb54a684ac9b6d
unknown <Rezadi Falah>
2021-05-25 23:48:55 +0700

    add Landing

---
+++ client/src/components/Hero/Hero.js
@@ -0,0 +1,35 @@
+import React, {useState, useContext} from 'react'
+import { Link, useHistory } from 'react-router-dom';
+import { Context } from '../../context/context';
+import ModalLogin from '../Modal/ModalLogin';
+
+function Hero() {
+
+    const [ state, ] = useContext(Context)
+    const [isOpen, setIsOpen] = useState(false)
+    const router = useHistory();
+
+    return (
+        <div>
+           <div className='hero-container'>
+                <div className='hero'>
+                    <div className='hero-content'>
+                        <h3>Deadpool</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        {!state.isLogin ? (
+                            <>
+                                <div className='hero-link' onClick={() => {setIsOpen(true)}}>Buy Now</div>
+                                <ModalLogin open={isOpen} onClose={() => setIsOpen(false)}></ModalLogin>
+                            </>
+                        ) : (
+                            <div className='hero-link' onClick={() => router.push("/film/:id")}>Buy Now</div>
+                        )}
+                        
+                    </div>
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+export default Hero
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,45 @@
 import React, {useContext, useEffect, useState} from 'react'
 import { useHistory } from "react-router-dom";
+import Hero from '../../components/Hero/Hero';
+import Card from '../../components/Card/Card';
+import {API} from '../../config/api';
 
 function Home() {
 
     const router = useHistory();
 
-    
+    const [ films, setFilms ]= useState([]);
+
+    const loadFilms = async () => {
+        try {
+            const response = await API.get("/films");
+            setFilms(response.data.data.films);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        loadFilms();
+    }, []);
 
     return (
         <>
-            
+         <div className="container mt-5">
+                <Hero />
+                <div className='card '>
+                <div className='card-body card-fund'>
+                    <div className='row'>
+                        {films&&films.map((film, index) => (
+                            <div className='col' key={film.id + index}>
+                                <Card film = { film } />
+                            </div>
+                        ))}
+                        
+                    </div>
+                </div>
+                </div>
+            </div>   
         </>
     )
 }
